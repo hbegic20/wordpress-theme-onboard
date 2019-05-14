3dd87cf60420c92d670281992a476f709efd34c8
Shekey <ajdin.sahinbegovic@p-m.si>
2019-05-14 09:41:21 +0200

    register menu location

--- wp-content/themes/onBoardingTheme/inc/register-menu-location.php
+++ wp-content/themes/onBoardingTheme/inc/register-menu-location.php
@@ -2,8 +2,7 @@
 <?php
   function register_custom_nav_menus() {
     register_nav_menus( array(
-      'pluginbuddy_mobile' => 'PluginBuddy Mobile Navigation Menu',
-      'footer_menu' => 'My Custom Footer Menu',
+      'header' => 'Header Navigation Menu',
     ) );
   }
 ?>
